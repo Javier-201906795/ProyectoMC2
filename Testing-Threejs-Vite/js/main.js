aaa32c47a4d043bc56ae9569f60ba30277b8fef7
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2024-09-11 23:09:44 -0600

    luz volumetrica

--- Testing-Threejs-Vite/js/main.js
+++ Testing-Threejs-Vite/js/main.js
@@ -4,11 +4,16 @@ import scene from './basic/Scene.js';
 import camera from './basic/Camera.js';
 import renderer from './basic/Render.js';
 import cube from './shapes/Cube.js';
+import light from './basic/Ligth.js';
+import directionalLight from './basic/DirectionLigth.js';
 
 
 console.log(scene, camera, renderer, cube);
 
 scene.add( cube );
+scene.add( light );
+scene.add( directionalLight );
+
 camera.position.set(2,2,2)
 camera.lookAt(cube.position)
 
