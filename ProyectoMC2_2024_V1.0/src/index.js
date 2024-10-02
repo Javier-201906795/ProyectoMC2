53fff72d55d01c253098e23eb3d2d123ef78d01a
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2024-10-01 19:00:49 -0600

    actualizar verision del programa

--- ProyectoMC2_2024_V1.0/src/index.js
+++ ProyectoMC2_2024_V1.0/src/index.js
@@ -16,7 +16,7 @@ import keyCode from "../js/controller/KeyCode.js";
 //PROYECTO MC2 2024
 //ALUMNO: Javier Ricardo Yllescas Barrios
 //CARNE: 201906795
-//Version Programa: V2.1.2
+//Version Programa: V2.2.0
 //{ Funciona con 3, 9, 15 cartas }
 
 
