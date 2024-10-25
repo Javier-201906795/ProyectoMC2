9402016701b01de0291f68a3f3b2b02f0f9b6d02
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2024-10-24 22:32:07 -0600

    zoom carta 33

--- ProyectoMC2_2024_V1.0/src/index.js
+++ ProyectoMC2_2024_V1.0/src/index.js
@@ -186,7 +186,7 @@ function final(){
   if (!flagfinal){
     //camara
     let numerodecartas = parseInt(cardnumber)
-    camera.position.z = numerodecartas === 21 ? 7: numerodecartas === 27 ? 8: numerodecartas === 33 ? 5: numerodecartas === 39 ? 10: 5;
+    camera.position.z = numerodecartas === 21 ? 7: numerodecartas === 27 ? 8: numerodecartas === 33 ? 10: numerodecartas === 39 ? 10: 5;
     console.log("zoom", camera.position.z )
     camera.position.y = numerodecartas === 21 ? caminiy: numerodecartas === 27 ? caminiy: numerodecartas === 33 ? caminiy: numerodecartas === 39 ? caminiy+2: caminiy;
     //obtener numero de carta
@@ -456,8 +456,8 @@ function animate() {
             let ajustey = numerodecartas === 9 ? 2 : numerodecartas === 15 ? 1 :numerodecartas === 21 ? 0.5: numerodecartas === 27 ? 0.1: numerodecartas === 33 ? 0.25: numerodecartas === 39 ? 0.25: 0;
             console.log("conty",conty,"ajustey",ajustey)
             //camara
-            let zoom = numerodecartas === 21 ? 8: numerodecartas === 27 ? 8 :numerodecartas === 33 ? 10: numerodecartas === 39 ? 10: caminiz;
-            let camy = numerodecartas === 21 ? -3.5: numerodecartas === 27 ? -1: numerodecartas === 33 ? -2.5:numerodecartas === 39 ? -5.5: 0;
+            let zoom = numerodecartas === 21 ? 8: numerodecartas === 27 ? 8 :numerodecartas === 33 ? 9: numerodecartas === 39 ? 10: caminiz;
+            let camy = numerodecartas === 21 ? -3.5: numerodecartas === 27 ? -1: numerodecartas === 33 ? -4.5:numerodecartas === 39 ? -5.5: 0;
             camera.position.z = zoom;
             camera.position.y = caminiy + camy;
             //ordenar cartas
@@ -817,7 +817,7 @@ function animate() {
             let contz = 0
             let ajustey = numerodecartas === 9 ? 2 : numerodecartas === 15 ? 1 : numerodecartas === 21 ? 0.5: numerodecartas === 27 ? 0.1:  numerodecartas === 39 ? -0.05:0;
             //camara
-            let camy = numerodecartas === 21 ? 2: numerodecartas === 27 ? -0.5: numerodecartas === 33 ? -2.5: numerodecartas === 39 ? 6: 0;
+            let camy = numerodecartas === 21 ? 2: numerodecartas === 27 ? -0.5: numerodecartas === 33 ? 4: numerodecartas === 39 ? 6: 0;
             camera.position.y = caminiy - camy;
             console.log("caminiy", caminiy, "camy", camy, "total", caminiy - camy)
             //cartas
@@ -895,9 +895,9 @@ function animate() {
             let cont = 0
             let conty = numerodecartas === 21 ? -6: numerodecartas === 33 ? -8: -3
             let contz = 0
-            let ajustey = numerodecartas === 9 ? 2 : numerodecartas === 15 ? 1 : numerodecartas === 21 ? 0.5: numerodecartas === 33 ? 0.25: 0;
+            let ajustey = numerodecartas === 9 ? 2 : numerodecartas === 15 ? 1 : numerodecartas === 21 ? 0.5: numerodecartas === 33 ? 0.1: 0;
             //camara
-            let camy = numerodecartas === 21 ? -2: numerodecartas === 27 ? -0.5: numerodecartas === 33 ? -2.5: numerodecartas === 39 ? 4: 0;
+            let camy = numerodecartas === 21 ? -2: numerodecartas === 27 ? -0.5: numerodecartas === 33 ? -3.5: numerodecartas === 39 ? 4: 0;
             camera.position.y = caminiy + camy;
             //cartas
             for (let i = 0; i < ordenvaraja.length ; i++) {
